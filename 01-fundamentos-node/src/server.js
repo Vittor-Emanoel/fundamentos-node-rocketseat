4777073f00a568b16f67cf3b2540de35846db85e
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-04-24 15:51:12 -0300

    rotas

--- 01-fundamentos-node/src/server.js
+++ 01-fundamentos-node/src/server.js
@@ -1,5 +1,17 @@
 import http from "node:http";
 
-const server = http.createServer((request, response) => {});
+const server = http.createServer((request, response) => {
+  const { method, url } = request;
+
+  if (method === "GET" && url === "/users") {
+    return response.end("listagem de user");
+  }
+
+  if (method === "POST" && url === "/users") {
+    return response.end("Criação de usuário");
+  }
+
+  return response.end("Hello World");
+});
 
 server.listen(3333);
